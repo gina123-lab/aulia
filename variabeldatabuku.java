fa3f8ed9c653ab32f9e9229bd0e0311118ed74c1
gina123-lab <147288612+gina123-lab@users.noreply.github.com>
2023-11-03 23:00:12 +0700

    Add files via upload

---
+++ variabeldatabuku.java
@@ -0,0 +1,15 @@
+public class Variabeldatabuku {
+    String Judul;
+    String Pengarang;
+    String Penerbit;
+    int Kategori;
+    int Tahun;
+    public databuku(String Judul, String Pengarang, String Penerbit, int Kategori, int Tahun) {
+        this.Judul = Judul;
+        this.Pengarang = Pengarang;
+        this.Penerbit = Penerbit;
+        this.Kategori = Kategori;
+        this.Tahun = Tahun;
+    }
+}
+
